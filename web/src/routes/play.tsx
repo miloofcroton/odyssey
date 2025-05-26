222e6b2d6add43641bb6d2fa20a87ae785c1e247
Jack Toumey <jack@toumey.io>
2025-05-25 21:57:49 -0700

    adding placeholders

---
+++ web/src/routes/play.tsx
@@ -0,0 +1,35 @@
+import { css } from '@linaria/core';
+
+import type { Route } from './+types/play';
+
+export const meta = ({}: Route.MetaArgs) => {
+  return [
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ];
+};
+
+const PlayPage = () => {
+  return (
+    <section className={css`
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      flex-grow: 1;
+      padding: 50px;
+      height: 600px;
+
+      @media (max-width: 700px) {
+        grid-template-columns: 1fr;
+        padding: 0;
+
+        span {
+          display: inline-block;
+        }
+      }
+    `}>
+      Play
+    </section>
+  );
+};
+
+export default PlayPage;
