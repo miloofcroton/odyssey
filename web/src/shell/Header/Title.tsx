bb722c2be367b52e9ddd7f5ca0938d9fd1e8a203
Jack Toumey <jack@toumey.io>
2025-05-25 21:58:01 -0700

    spacing

--- web/src/shell/Header/Title.tsx
+++ web/src/shell/Header/Title.tsx
@@ -3,10 +3,14 @@ import { css } from '@linaria/core';
 const SITE_TITLE = 'Jack Toumey';
 
 const Title = () => (
-  <h1 className={css`
-    font-size: 30px;
-    line-height: 2;
-  `}>{SITE_TITLE}</h1>
+  <h1
+    className={css`
+      font-size: 30px;
+      line-height: 2;
+    `}
+  >
+    {SITE_TITLE}
+  </h1>
 );
 
 export default Title;
